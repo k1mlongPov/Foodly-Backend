5f71b8a8da7a17f34a8f090de1d9d646234b2108
longzz12 <kimlong26012004@gmail.com>
2025-10-11 09:49:14 +0700

    Initial commit

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, require: true },
     email: { type: String, require: true, unique: true },
     otp: { type: String, require: false, default: "none" },
+    fcm: { type: String, require: false, default: "none" },
     password: { type: String, require: true },
     verification: { type: Boolean, default: false },
     phone: { type: String, default: "0123456789" },
